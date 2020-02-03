5888d4616b3bd6da8c7a25bddfffc5a1d2dfa11c
Anastasiya Sliunkova <asynkova@marathonbet.ru>
2020-02-03 14:29:23 +0300

    Добавление @Value и подключение к Spring Boot Sleuth

---
+++ src/main/java/com/example/springBoot/AppController.java
@@ -0,0 +1,31 @@
+package com.example.springBoot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class AppController {
+    @Autowired
+    private EmailService emailService;
+
+    static final Logger LOGGER = LoggerFactory.getLogger(AppController.class);
+
+    @RequestMapping("/greeting")
+    public void greeting(@RequestParam(value = "email") String email) {
+        emailService.sendEmail(email);
+        LOGGER.info("Received email: " + email);
+    }
+
+    @GetMapping("/")
+    public String helloSleuth() {
+        LOGGER.info("Hello Sleuth");
+        return "success";
+    }
+
+}
--- src/main/java/com/example/springBoot/RabbitConfiguration.java
+++ src/main/java/com/example/springBoot/RabbitConfiguration.java
@@ -1,10 +1,13 @@
-package com.example.springrabbitmq;
+
+package com.example.springBoot;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,4 +21,26 @@ public class RabbitConfiguration {
         return new SimpleMessageListenerContainer(connectionFactory);
     }
 
+    @Value("${spring.rabbitmq.template.default-receive-queue}")
+    private String QueueName;
+
+    @Bean
+    public Queue myQueue1() {
+        return new Queue(QueueName, true);
+    }
+
+    @Value("${spring.rabbitmq.template.exchange}")
+    private String ExchangeName;
+
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange(ExchangeName);
+    }
+
+    @Bean
+    public Binding errorBinding1() {
+        return BindingBuilder.bind(myQueue1()).to(directExchange()).with("error");
+    }
+
 }
+
