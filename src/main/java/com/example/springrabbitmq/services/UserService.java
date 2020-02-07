1fe799ddeb20c2bce47752d59a0daa821cec6087
Anastasiya Sliunkova <asynkova@marathonbet.ru>
2020-02-07 13:04:19 +0300

    Добавление проверки количества запросов и добавление флага

--- src/main/java/com/example/springrabbitmq/services/UserService.java
+++ src/main/java/com/example/springrabbitmq/services/UserService.java
@@ -2,12 +2,19 @@ package com.example.springrabbitmq.services;
 
 import com.example.springrabbitmq.entity.UserRequest;
 import com.example.springrabbitmq.repository.UserRequestRepository;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class UserService {
+    @Value("${spring.rabbitmq.template.exchange}")
+    private String exchangeName;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     private final UserRequestRepository userRequestRepository;
 
@@ -23,5 +30,9 @@ public class UserService {
         return userRequestRepository.findByEmail(email);
     }
 
+
+    public void sendEmailIfMore(String email) {
+        rabbitTemplate.convertAndSend(exchangeName, "info", email);
+    }
 
 }
